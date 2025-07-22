e39d10df7f5432ad82b899c2a627e41e47cbb42a
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-07-22 09:19:34 +0400

    Add Angular

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DxButtonModule } from 'devextreme-angular/ui/button';
+import { DxFileUploaderModule } from 'devextreme-angular/ui/file-uploader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DxButtonModule,
+    DxFileUploaderModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
