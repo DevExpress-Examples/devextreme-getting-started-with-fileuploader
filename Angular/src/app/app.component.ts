5e9c2b93cc80a0615f4060425a321da9865ea30c
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 13:37:04 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ClickEvent } from 'devextreme/ui/button';
 
 import { DxFileUploaderModule } from 'devextreme-angular/ui/file-uploader';
@@ -7,6 +7,7 @@ import { DxFileUploaderModule } from 'devextreme-angular/ui/file-uploader';
     selector: 'app-root',
     imports: [DxFileUploaderModule],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
